b6eb0e229b4a79ca96520283e976d4c2b23dfa9c
h3nr1-g <glueck.h@googlemail.com>
2015-08-14 11:25:34 +0200

    phase3 begonnen

--- phase3/DimAlter.sql
+++ phase3/DimAlter.sql
@@ -1,22 +1,37 @@
-INSERT INTO [Cubes].[dbo].[DimAlter](Bezeichnung, Anzahl)
-Values
-(
-'<20', (select isnull(sum(b.anzahl), 0)
-from
--- OPTIMIERUNG: Diese Query als Materialized View speichern
-	(select a.age, count(a.riskID) 'anzahl' 
-	from
-		(select distinct riskID, DATEDIFF(yy, birthdate, CURRENT_TIMESTAMP) 'age'
-		from [DWH-OnlineShop].dbo.iw_customer
-		group by riskID, birthdate) a
-	group by a.age) b
-where b.age < 20)
-)
+USE Cubes;
 
-INSERT INTO [Cubes].[dbo].[DimAlter](Bezeichnung, Anzahl)
+DECLARE @anzahl int;
+SET @anzahl = 0;
+
+DECLARE @altersgruppen TABLE(gruppenname VARCHAR(60), bedingung VARCHAR(100));
+INSERT INTO @altersgruppen (gruppenname, bedingung)
+  VALUES (1, 'Jan'), (2, 'Feb'), (3, 'Mar');
+
+
+	BEGIN
+		INSERT INTO [DimAlter](Bezeichnung, Anzahl)
+		Values(
+			@groupName, (select isnull(sum(b.anzahl), 0)
+			from
+			-- OPTIMIERUNG: Diese Query als Materialized View speichern
+				(select a.age, count(a.riskID) 'anzahl' 
+				from
+					(select distinct riskID, DATEDIFF(yy, birthdate, CURRENT_TIMESTAMP) 'age'
+					from [DWH-OnlineShop].dbo.iw_customer
+					group by riskID, birthdate) a
+				group by a.age) b
+			where b.age < 20)
+		)
+	END
+
+	
+
+
+
+INSERT INTO [DimAlter](Bezeichnung, Anzahl)
 Values
 (
-'>=20 & < 30', (select isnull(sum(b.anzahl), 0)?
+'>=20 & < 30', (select isnull(sum(b.anzahl), 0)
 from
 	(select a.age, count(a.riskID) 'anzahl' 
 	from
@@ -28,7 +43,7 @@ where b.age >= 20 and b.age < 30)
 )
 
 
-INSERT INTO [Cubes].[dbo].[DimAlter](Bezeichnung, Anzahl)
+INSERT INTO [DimAlter](Bezeichnung, Anzahl)
 Values
 (
 '>=30 & < 40', (select isnull(sum(b.anzahl), 0)
@@ -43,7 +58,7 @@ where b.age >= 30 and b.age < 40)
 )
 
 
-INSERT INTO [Cubes].[dbo].[DimAlter](Bezeichnung, Anzahl)
+INSERT INTO [DimAlter](Bezeichnung, Anzahl)
 Values
 (
 '>=40 & < 50', (select isnull(sum(b.anzahl), 0)
@@ -57,7 +72,7 @@ from
 where b.age >= 40 and b.age < 50)
 )
 
-INSERT INTO [Cubes].[dbo].[DimAlter](Bezeichnung, Anzahl)
+INSERT INTO [DimAlter](Bezeichnung, Anzahl)
 Values
 (
 '>=50 & < 60', (select isnull(sum(b.anzahl), 0)
@@ -71,7 +86,7 @@ from
 where b.age >= 50 and b.age < 60)
 )
 
-INSERT INTO [Cubes].[dbo].[DimAlter](Bezeichnung, Anzahl)
+INSERT INTO [DimAlter](Bezeichnung, Anzahl)
 Values
 (
 '>=60 & < 90', (select isnull(sum(b.anzahl), 0)
@@ -85,7 +100,7 @@ from
 where b.age >= 60 and b.age < 90)
 )
 
-INSERT INTO [Cubes].[dbo].[DimAlter](Bezeichnung, Anzahl)
+INSERT INTO [DimAlter](Bezeichnung, Anzahl)
 Values
 (
 'ohne gültige Altersangabe', (select isnull(sum(b.anzahl), 0)
